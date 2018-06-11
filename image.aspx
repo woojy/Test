bee6b597437d83c8b95dcdc8e3a5fd649c37a73c
Jaeyun Woo <woojy3770@e-mirim.hs.kr>
2018-06-11 20:49:41 +0900

    image upload

---
+++ image.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="image.aspx.cs" Inherits="metoo1.image" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" method="post">
+        <div>
+            후보사진 등록 : <input type="file" name="Image"/><br />
+        </div>
+        <asp:Button ID ="btnSqlConnection" runat="server" 
+            Text="OK" OnClick="btn_Click"/>
+        <hr />
+    </form>
+
+    <asp:Label ID="aa" runat="server"></asp:Label> <br />
+    <asp:Label ID="dbcheck" runat="server"></asp:Label>
+</body>
+</html>
---
+++ image.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace metoo1
+{
+    public partial class image : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btn_Click(object sender, EventArgs e)
+        {
+
+            string Image = Request.Form["Image"];
+            int ImageNo= 0;
+            aa.Text = Image;  
+            string connectionString = "Data Source=10.96.124.87,1433;Initial Catalog=metoo;Persist Security Info=True;User ID=sa;Password=metoo";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("INSERT INTO IMAGE VALUES(@ImageNo,@ImageName, @ImageSize);", conn)) //imageNo는 기본키
+                    {
+                        cmd.Parameters.AddWithValue("@ImageNo", ImageNo);
+                        cmd.Parameters.AddWithValue("@ImageName", Image);
+                        cmd.Parameters.AddWithValue("@ImageSize", 5);
+
+                        int row = cmd.ExecuteNonQuery();
+                        dbcheck.Text = "연결완료!";
+                    }
+                    conn.Close();
+                }
+            }
+            catch (SqlException ex)
+            {
+                dbcheck.Text = ex.Message;
+            }
+        }
+    }
+}
